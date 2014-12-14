424a0d4b688245203b3b54366b6ef7b230ff035a
Mark Finger <markfinger@gmail.com>
2014-12-14 18:59:44 +1100

    PyPI tweaks.

--- setup.py
+++ setup.py
@@ -20,13 +20,8 @@ setup(
         'django-node >= 2.0.1',
         'django-webpack >= 1.0.1',
     ],
-    description='Django React',
-    long_description=\
-'''
-Render and bundle React components from a Django application.
-
-Documentation at https://github.com/markfinger/django-react
-''',
+    description='Render and bundle React components from a Django application',
+    long_description='Documentation at https://github.com/markfinger/django-react',
     author='Mark Finger',
     author_email='markfinger@gmail.com',
     url='https://github.com/markfinger/django-react',
