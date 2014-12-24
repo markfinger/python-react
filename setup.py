4d902ff4fc3deb808a2acec1094d0c810d6778fd
Mark Finger <markfinger@gmail.com>
2014-12-24 18:48:36 +1100

    Bumping the requirements for django-node and django-webpack.

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ setup(
     },
     install_requires=[
         'django',
-        'django-node >= 2.0.1',
-        'django-webpack >= 1.0.1',
+        'django-node >= 2.1.0',
+        'django-webpack >= 1.1.0',
     ],
     description='Render and bundle React components from a Django application',
     long_description='Documentation at https://github.com/markfinger/django-react',
