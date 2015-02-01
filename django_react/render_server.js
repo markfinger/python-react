49a70cec774e309c053ce6e4fc12685e3e75f549
Mark Finger <markfinger@gmail.com>
2015-02-01 16:09:33 +1100

    Cleanup.

--- django_react/render_server.js
+++ django_react/render_server.js
@@ -8,12 +8,6 @@ var nodeJSX = require('node-jsx');
 // Support requiring of JSX files
 nodeJSX.install();
 
-// `render-to` param options
-var RENDER_TO_STATIC = 'static';
-var RENDER_TO_STRING = 'string';
-
-var app = express();
-
 var address = argv.address;
 if (address === undefined) {
 	throw new Error('No address was provided, ex: `--address 127.0.0.1`');
@@ -23,6 +17,8 @@ var port = argv.port;
 if (port === undefined) {
 	throw new Error('No port was provided, ex: `--port 0`');
 }
+
+var app = express();
 
 var server = app.listen(port, address, function() {
 	console.log('Started django-react render server');
@@ -34,6 +30,10 @@ app.get('/', function(req, res) {
 	res.send('django-react render server');
 });
 
+// `render-to` param options
+var RENDER_TO_STATIC = 'static';
+var RENDER_TO_STRING = 'string';
+
 app.get('/render', function(req, res) {
 	var pathToSource = req.query['path-to-source'];
 	if (!pathToSource) {
@@ -44,15 +44,6 @@ app.get('/render', function(req, res) {
 		throw new Error('The file specified by path-to-source, "' + pathToSource + '", cannot be found.');
 	}
 	var component = require(pathToSource);
-
-	var renderTo = req.query['render-to'];
-	if (!renderTo) {
-		throw new Error('No render-to option was provided, ex: `?render-to=' + RENDER_TO_STATIC + '` or `?render-to=' + RENDER_TO_STRING + '`');
-	}
-	renderTo = decodeURIComponent(renderTo);
-	if (renderTo !== RENDER_TO_STATIC && renderTo !== RENDER_TO_STRING) {
-		throw new Error('Unknown render-to option "' + renderTo + '", only "?render-to=' + RENDER_TO_STATIC + '" and "?render-to=' + RENDER_TO_STRING + '" are accepted');
-	}
 
 	var props = undefined;
 	var pathToSerializedProps = req.query['path-to-serialized-props'];
@@ -67,11 +58,19 @@ app.get('/render', function(req, res) {
 
 	var element = React.createElement(component, props);
 
+	var renderTo = req.query['render-to'];
+	if (!renderTo) {
+		throw new Error('No render-to option was provided, ex: `?render-to=' + RENDER_TO_STATIC + '` or `?render-to=' + RENDER_TO_STRING + '`');
+	}
+	renderTo = decodeURIComponent(renderTo);
+
 	var output;
 	if (renderTo === RENDER_TO_STATIC) {
 		output = React.renderToStaticMarkup(element);
 	} else if (renderTo === RENDER_TO_STRING) {
 		output = React.renderToString(element);
+	} else {
+		throw new Error('Unknown render-to option "' + renderTo + '", only "?render-to=' + RENDER_TO_STATIC + '" and "?render-to=' + RENDER_TO_STRING + '" are accepted');
 	}
 
 	res.send(output);
