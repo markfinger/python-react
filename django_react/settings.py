78f5cddcaed23c5a65aa931d47cdbb6e48ad0497
Mark Finger <markfinger@gmail.com>
2015-02-16 21:06:32 +1100

    Removing some legacy settings which are no longer used.

--- django_react/settings.py
+++ django_react/settings.py
@@ -2,16 +2,6 @@ from django.conf import settings
 
 setting_overrides = getattr(settings, 'DJANGO_REACT', {})
 
-STATIC_ROOT = setting_overrides.get(
-    'STATIC_ROOT',
-    settings.STATIC_ROOT,
-)
-
-STATIC_URL = setting_overrides.get(
-    'STATIC_URL',
-    settings.STATIC_URL,
-)
-
 REACT_EXTERNAL = setting_overrides.get(
     'REACT_EXTERNAL',
     'window.React',
