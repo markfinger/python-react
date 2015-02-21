a149040964cf0051f7bd1688089b6e37c0e54bd4
Matthew Scott <scott@fundinggates.com>
2015-02-21 16:31:05 -0600

    Expose React as window.React on client

--- example/example/static/example/HelloWorld.jsx
+++ example/example/static/example/HelloWorld.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 
+if (typeof window !== 'undefined') { window.React = React; }
+
 var HelloWorld = React.createClass({
     render: function() {
         return <span>Hello, {this.props.name}!</span>;
