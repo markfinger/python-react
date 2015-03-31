09d8dc9e8130ffd356137348303d8dc5288f8537
Mark Finger <markfinger@gmail.com>
2015-04-01 10:55:34 +1100

    Pinned the requirements.

--- setup.py
+++ setup.py
@@ -15,8 +15,7 @@ setup(
     },
     install_requires=[
         'django',
-        'django-node==3.0.0',
-        'django-webpack==2.1.2',
+        'django-node==4.0.0'
     ],
     description='Render and bundle React components from a Django application',
     long_description='Documentation at https://github.com/markfinger/django-react',
