dbb7c88ced953fea178a892f29e1ea30b3037aa4
Mark Finger <markfinger@gmail.com>
2015-05-04 23:19:57 +1000

    Updated dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     version=react.__version__,
     packages=['django_react'],
     install_requires=[
-        'optional-django==0.2.1'
+        'webpack==4.0.1',
+        'optional-django==0.3.0',
     ],
     description='Server-side rendering, client-side mounting, JSX translation, and component bundling',
     long_description='Documentation at https://github.com/markfinger/python-react',
