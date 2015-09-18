9336017c0b5648f4da3aae73ec12b103878a073a
ALIANE Abdelouahab <abdelouahab_al@yahoo.fr>
2015-09-18 18:56:42 +0100

    Create CommentForm.jsx

---
+++ examples/Tornado-example/static/js/CommentForm.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class CommentForm extends React.Component {
+	render() {
+		return (
+			<form method="post" action={this.props.url}>
+			<input type="hidden" name="_xsrf" value={this.props.xsrf}/>
+				<h2>Submit a comment</h2>
+				<div className="form-group">
+					<label>
+						Your hello
+						<input name="author" type="text" className="form-control" placeholder="..." />
+					</label>
+				</div>
+				<div className="form-group">
+					<label>
+						Say something...
+						<textarea name="text" className="form-control" placeholder="..." />
+					</label>
+				</div>
+				<div className="text-right">
+					<button type="reset" className="btn btn-default">Reset</button>
+					<button type="submit" className="btn btn-primary">Submit</button>
+				</div>
+			</form>
+		);
+	}
+}
+
+export default CommentForm;
